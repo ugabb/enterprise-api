0bc5a9cea91f011de72415fca99fceee2baee751
Gabriel Barros <ugabrieldev@gmai.com>
2024-05-22 22:35:34 -0300

    feat: Add support for additional HTTP methods in CORS configuration

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,8 @@ import { env } from "./env";
 export const app = express();
 
 app.use(cors({
-    origin: env.FRONT_END_ORIGIN
+    origin: env.FRONT_END_ORIGIN,
+    methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 app.use(express.json());
 app.use(router);
